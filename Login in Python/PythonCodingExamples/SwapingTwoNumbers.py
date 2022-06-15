ac5ee60c5625598dfda9a41589912654213a02ae
CheTmkGitHub <88035327+CheTmkGitHub@users.noreply.github.com>
2022-06-15 09:38:12 +0530

    Add files via upload

---
+++ Login in Python/PythonCodingExamples/SwapingTwoNumbers.py
@@ -0,0 +1,9 @@
+num1 = 10
+num2 = 20
+print("Value of number1 before swapping:",num1)
+print("Value of number2 before swapping:",num2)
+temp = num1
+num1 = num2
+num2 = temp
+print("Value of number1 before swapping:",num1)
+print("Value of number2 before swapping:",num2)
